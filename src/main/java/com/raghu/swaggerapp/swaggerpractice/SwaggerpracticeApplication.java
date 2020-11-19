b972f6ccb3393e8775033f405108587b584e0176
raghu8 <rvmanthena@gmail.com>
2020-11-19 10:41:00 -0500

    All Files

---
+++ src/main/java/com/raghu/swaggerapp/swaggerpractice/SwaggerpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.raghu.swaggerapp.swaggerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerpracticeApplication.class, args);
+	}
+
+}
